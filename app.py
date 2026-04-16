85cdc54d18084ed7a29cbf9e01dfea01b1d194bb
Krushna Koshatwar <krushnakoshatwaar619@gmail.com>
2026-04-16 22:31:38 +0530

    fix deployment

--- app.py
+++ app.py
@@ -20,4 +20,8 @@ def home():
 
     return render_template("index.html", result=result)
 
-app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
